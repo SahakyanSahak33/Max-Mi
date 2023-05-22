e59cc78364f359c3781ec0519f27cb245ad6f077
Sahak <sahakyansahak303@gmail.com>
2023-05-22 18:27:22 +0400

    Update V1__init_migration.sql

--- src/main/resources/db/migration/V1__init_migration.sql
+++ src/main/resources/db/migration/V1__init_migration.sql
@@ -29,21 +29,34 @@ CREATE TABLE user_authority (
                                 FOREIGN KEY (user_id) REFERENCES users(id),
                                 FOREIGN KEY (authority_id) REFERENCES authorities(id)
 );
+
+CREATE TABLE products (
+                          product_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          product_name VARCHAR(100),
+                          price INT,
+                          description TEXT,
+                          image BLOB
+);
+CREATE TABLE orders (
+                        order_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        user_id BIGINT,
+                        order_date DATE NOT NULL,
+                        product_id BIGINT,
+                        address VARCHAR(250) NOT NULL,
+                        city VARCHAR(250) NOT NULL,
+                        country VARCHAR(250) NOT NULL,
+                        FOREIGN KEY (product_id) REFERENCES products(product_id),
+                        FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+CREATE TABLE liked_products (
+                                like_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                user_id BIGINT,
+                                product_id BIGINT,
+                                FOREIGN KEY (user_id) REFERENCES users(id),
+                                FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
 INSERT INTO `my_db`.`authorities` (`name`) VALUES ('USER');
 INSERT INTO `my_db`.`authorities` (`name`) VALUES ('ADMIN');
-
-/*
-CREATE TABLE `my_db`.`user_details` (
-                                        `id` BIGINT NOT NULL AUTO_INCREMENT,
-                                        `username` VARCHAR(255) NOT NULL,
-                                        `first_name` VARCHAR(255) NOT NULL,
-                                        `last_name` VARCHAR(255) NOT NULL,
-                                        `email` VARCHAR(255) NOT NULL,
-                                        `phone_number` VARCHAR(255) NOT NULL,
-                                        `date` VARCHAR(255) NOT NULL,
-                                        `gender` VARCHAR(45) NOT NULL,
-                                        PRIMARY KEY (`id`),
-                                        UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
-                                        UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-                                        UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC) VISIBLE ,
-                                        FOREIGN KEY (username) references users(username));*/
